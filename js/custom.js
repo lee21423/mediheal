8229cbbe1f386206708b3664cd6fd416cf81af8d
lee21423 <dbwls5676@naver.com>
2024-06-19 17:40:47 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+$(function () {
+    $('.main_visual_slide').slick({
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+
+    const mainContentSlide = new Swiper('.main-content-slide', {
+        loop: true,
+        slidesPerView: 4,
+        spaceBetween: 30,
+    })
+
+});
+
+
+
+
+
+
+
